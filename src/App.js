20b5b501b2a68e47e14f5cc4d9da06bb7b4d1e0a
andriydobrosynets <andriy.andriy1995@gmail.com>
2021-08-19 01:31:28 +0300

    lesson4 HW4

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import FormAddCar from "./components/formAddCar/FormAddCar";
+import Cars from "./components/cars/Cars";
 
 function App() {
   return (
     <div>
-
+      <FormAddCar/>
+      <Cars/>
     </div>
   );
 }
