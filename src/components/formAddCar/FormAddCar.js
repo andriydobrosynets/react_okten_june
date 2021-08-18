895bb22b70da678abb64dfa3b5eb59508b7ab7dc
andriydobrosynets <andriy.andriy1995@gmail.com>
2021-08-19 01:31:48 +0300

    lesson4 HW4

---
+++ src/components/formAddCar/FormAddCar.js
@@ -0,0 +1,31 @@
+import {useState} from "react";
+import {saveCar} from "../../services/car.save.service";
+
+export default function FormAddCar() {
+    let [formState, setFormState] = useState({model: '', price: '', year: ''});
+
+    let onFormInputChange = (e) => {
+        setFormState({...formState, [e.target.name]: e.target.value});
+    }
+
+    let save = (e) => {
+        e.preventDefault();
+        saveCar(formState);
+
+    }
+
+    return (
+        <div>
+            <form onSubmit={save}>
+                    Model Car
+                    <input type="text" name={'model'} value={formState.model} onChange={onFormInputChange}/>
+                    Price Car
+                    <input type="text" name={'price'} value={formState.price} onChange={onFormInputChange}/>
+                    Year Car
+                    <input type="text" name={'year'} value={formState.year} onChange={onFormInputChange}/>
+                <input type="submit" />
+            </form>
+
+        </div>
+    );
+}
