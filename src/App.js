cccac176c3f116e96c48b84396f2842af48b06b7
andriydobrosynets <andriy.andriy1995@gmail.com>
2021-08-26 15:51:38 +0300

    lesson7 HW7

--- src/App.js
+++ src/App.js
@@ -6,20 +6,23 @@ function reducer(state, action) {
     return{...state,a:state.a+10}
   }else if (action.obj === '1' && action.diya === '-') {
     return {...state, a: state.a-2}
-  }
+  }else if (action.obj === '1' && action.diya === '0') {
+      return {...state, a: state.a=0}}
   return {...state}
 }
 
 function App() {
   let [{a},dispatch] = useReducer(reducer,{a:0})
   return (
-    <div>
-      <h1>{a}</h1>
-      <div>
-          <button onClick={() => dispatch({obj: '1', diya: '+'})}>Додати 10</button>
-          <button onClick={() => dispatch({obj: '1', diya: '-'})}>Відняти 2</button>
-          <button onClick={() => {inerText}}>Відняти 2</button>
+    <div className={"full"}>
+      <h1 className={'lichilnik'}>{a}</h1>
+      <div className={'button'}>
+          <button className={'plus'} onClick={() => dispatch({obj: '1', diya: '+'})}>Додати 10</button>
+          <button className={'minus'} onClick={() => dispatch({obj: '1', diya: '-'})}>Відняти 2</button>
       </div>
+        <div className={'footer'}>
+            <button className={'null'} onClick={() => dispatch({obj: '1', diya: '0'})}>Очистити</button>
+        </div>
 
     </div>
   );
