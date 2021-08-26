1f58d183977d294793ebcd1ae0515770b16acc9f
andriydobrosynets <andriy.andriy1995@gmail.com>
2021-08-26 14:52:27 +0300

    lesson7 HW7

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import './App.css';
+import {useReducer} from "react";
+// import reducer from "./reducer/reducer";
+function reducer(state, action) {
+  if (action.obj ==='1' && action.diya === '+'){
+    return{...state,a:state.a+10}
+  }else if (action.obj === '1' && action.diya === '-') {
+    return {...state, a: state.a-2}
+  }
+  return {...state}
+}
 
 function App() {
+  let [{a},dispatch] = useReducer(reducer,{a:0})
   return (
     <div>
-
+      <h1>state 1 - {a}</h1>
+      <button onClick={() => dispatch({obj: '1', diya: '+'})}>Додати 10</button>
+      <button onClick={() => dispatch({obj: '1', diya: '-'})}>Відняти 2</button>
     </div>
   );
 }
---
+++ src/reducer/reducer.js
@@ -0,0 +1,9 @@
+// const reducer = (state, action) => {
+//     if (action.obj === '1' && action.diya === '+'){
+//         return{...state,a:state.a++}
+//     }else if (action.obj === '1' && action.diya === '-') {
+//         return {...state, a: state.a--}
+//     }
+//     return {...state}
+// }
+// export default reducer()
