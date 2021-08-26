a0018ba9bfa6b6d7a26f1df1eec08fddf831347e
andriydobrosynets <andriy.andriy1995@gmail.com>
2021-08-26 14:59:49 +0300

    lesson7 HW7

--- src/App.js
+++ src/App.js
@@ -14,9 +14,13 @@ function App() {
   let [{a},dispatch] = useReducer(reducer,{a:0})
   return (
     <div>
-      <h1>state 1 - {a}</h1>
-      <button onClick={() => dispatch({obj: '1', diya: '+'})}>Додати 10</button>
-      <button onClick={() => dispatch({obj: '1', diya: '-'})}>Відняти 2</button>
+      <h1>{a}</h1>
+      <div>
+          <button onClick={() => dispatch({obj: '1', diya: '+'})}>Додати 10</button>
+          <button onClick={() => dispatch({obj: '1', diya: '-'})}>Відняти 2</button>
+          <button onClick={() => {inerText}}>Відняти 2</button>
+      </div>
+
     </div>
   );
 }
